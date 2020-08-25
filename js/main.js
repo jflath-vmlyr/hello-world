394a4fefcd9f48b2816a32088fedcd719469f615
Jim Flath <Jim.Flath@vmlyr.com>
2020-08-25 16:17:21 -0500

    initial

---
+++ js/main.js
@@ -0,0 +1,43 @@
+
+function openTab(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+jQuery(document).ready(function( $ ) {
+
+		// Menu settings
+		$('#menuToggle, .menu-close').on('click', function(){
+			$('#menuToggle').toggleClass('active');
+			$('body').toggleClass('body-push-toleft');
+			$('#theMenu').toggleClass('menu-open');
+		});
+
+		// Smooth scroll for the menu and links with .scrollto classes
+	  $('.smoothscroll').on('click', function(e) {
+	    e.preventDefault();
+	    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      if (target.length) {
+
+	        $('html, body').animate({
+	          scrollTop: target.offset().top
+	        }, 1500, 'easeInOutExpo');
+	      }
+	    }
+			$('body').toggleClass('body-push-toleft');
+			$('#theMenu').toggleClass('menu-open');
+	  });
+
+});
+
